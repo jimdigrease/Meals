a051c9aa179c91f13fd51e5f0a13d3f83bd86a25
Jim <jim@gmail.com>
2022-11-22 17:16:13 +0200

    added Cart visibility state in App.js file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [isCartShown, setIsCartShown] = useState(false);
+
+  function showCartHandler() {
+    setIsCartShown(true);
+  }
+
+  function hideCartHandler() {
+    setIsCartShown(false);
+  }
+
   return (
     <CartProvider>
+      {isCartShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler} />
       <Header />
       <main>
         <Meals />
