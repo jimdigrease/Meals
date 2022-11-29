068fe212d2a1a87c291532b230a3a9452504e0dd
Jim <digreasejim@gmail.com>
2022-11-29 18:30:58 +0200

    added http-fetching meal items from database in AvailalbeMeals.js, also added loading state and handling errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,52 +1,92 @@
+import React, { useEffect, useState } from 'react';
+
+import styles from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
-import styles from './AvailableMeals.module.css';
+// Could use some outer constants for URLs
+//import { MEALS_URL } from '../../store/db-constants';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+function AvailableMeals() {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-function AvailableMeals () {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
+  useEffect(() => {
+    // Callback-function in useEffect should be synchronous only, but inside 
+	  // new function could be asynchronous instead.
+    async function fetchMeals() {
+      const response = await fetch('https://react-http-7919e-default-rtdb.europe-west1.firebasedatabase.app/Meals.json');
+        
+      if (!response.ok) {
+        throw new Error('Something went wrong! Error: ' + response.status);
+      }
+
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name, 
+          description: data[key].description, 
+          price: data[key].price
+        })
+      }
+      
+      setIsLoading(false);
+      setMeals(loadedMeals);
+    }
+    
+    // May wrap all function's body in try/catch block or use a traditional 
+    // Promise-only way here to catch an error when calling an async function. 
+    // Using try/catch for just calling fetchMeals() don't work, bacause of 
+    // async, need to wrap it in another async function and so on, but here 
+    // using Promise-catch method is a bit easier.
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);    
+    });
+  
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem 
+      key={meal.id} 
+      id={meal.id} 
       name={meal.name}
       description={meal.description}
       price={meal.price}
     />
   ));
 
-  return (
+  let content = 
     <section className={styles.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
-};
+      <p>Found no meals.</p>
+    </section>;
+
+  if (isLoading) {
+    content = 
+      <section className={styles.MealsLoading}>
+        <p>Loading...</p>
+      </section>;
+  }
+  if (mealsList.length > 0) {
+    content = 
+      <section className={styles.meals}>
+        <Card>
+          <ul>{mealsList}</ul>
+        </Card>
+      </section>;
+  }
+  if (error) {
+    content = 
+      <section className={styles.MealsError}>
+        <h2>{error}</h2>
+      </section>;
+  }
+
+  return content;
+}
 
 export default AvailableMeals;
